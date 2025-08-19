0cde5d1c8a928ae9975c1e3d74bd9f32f3f7bd76
Daher12 <133640261+Daher12@users.noreply.github.com>
2025-08-19 11:30:31 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,8 +6,8 @@ set -ouex pipefail
 
 
 #dnf5 install -y blueman NetworkManager-tui nautilus waybar xdg-user-dirs-gtk xdg-user-dirs file-roller kitty btop dunst tlp zsh zsh-syntax-highlighting brightnessctl rofi-wayland --setopt=install_weak_deps=False 
-dnf5 install -y NetworkManager-tui nautilus file-roller kitty tlp zsh zsh-syntax-highlighting  --setopt=install_weak_deps=False 
-dnf5 remove -y gnome-disk-utility thunar  thunar-archive-plugin thunar-volman xarchiver android-tools wlsunset kanshi swaylock swayidle swaybg tuned tuned-ppd
+#dnf5 install -y NetworkManager-tui nautilus file-roller kitty tlp zsh zsh-syntax-highlighting  --setopt=install_weak_deps=False 
+#dnf5 remove -y gnome-disk-utility thunar  thunar-archive-plugin thunar-volman xarchiver android-tools wlsunset kanshi swaylock swayidle swaybg tuned tuned-ppd
 
 ## Enable Ublue copr
 #dnf5 -y copr enable ublue-os/akmods 
@@ -18,22 +18,22 @@ dnf5 -y copr enable solopasha/hyprland
 dnf5 -y install hyprland hyprpaper hypridle hyprlock hyprshot  --setopt=install_weak_deps=False
 dnf5 -y copr disable solopasha/hyprland 
 
-dnf5 -y copr enable tofik/nwg-shell 
-dnf5 -y install nwg-look --setopt=install_weak_deps=False
-dnf5 -y copr disable tofik/nwg-shell 
+#dnf5 -y copr enable tofik/nwg-shell 
+#dnf5 -y install nwg-look --setopt=install_weak_deps=False
+#dnf5 -y copr disable tofik/nwg-shell 
 
-dnf5 -y copr enable chenxiaolong/sbctl 
-dnf5 -y install sbctl
-dnf5 -y copr disable chenxiaolong/sbctl 
+#dnf5 -y copr enable chenxiaolong/sbctl 
+#dnf5 -y install sbctl
+#dnf5 -y copr disable chenxiaolong/sbctl 
 
 
 ## Tailscale
-dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/Fedora_Rawhide/shells:zsh-users:zsh-autosuggestions.repo
-dnf5 -y install tailscale zsh-autosuggestions greetd tuigreet
+#dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+#dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/Fedora_Rawhide/shells:zsh-users:zsh-autosuggestions.repo
+#dnf5 -y install tailscale zsh-autosuggestions greetd tuigreet
 
-rm /etc/yum.repos.d/tailscale.repo
-rm /etc/yum.repos.d/shells:zsh-users:zsh-autosuggestions.repo
+#rm /etc/yum.repos.d/tailscale.repo
+#rm /etc/yum.repos.d/shells:zsh-users:zsh-autosuggestions.repo
 
 
 ## Nix
@@ -44,9 +44,7 @@ mkdir -p /nix && \
  curl -L https://github.com/curlpipe/ox/releases/latest/download/ox -o /usr/bin/ox && \
  chmod +x /usr/bin/ox
 
-dnf5 -y autoremove
-
-systemctl enable tlp
-systemctl enable tailscaled
+#systemctl enable tlp
+#systemctl enable tailscaled
 #systemctl enable greetd
 
