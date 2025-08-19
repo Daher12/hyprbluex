0d432d4b6be3c78e534833adb7b4f67e85e1ce6b
Daher12 <133640261+Daher12@users.noreply.github.com>
2025-08-19 12:22:22 +0200

    Revert "Wayblue"

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,18 +4,15 @@ set -ouex pipefail
 
 ### Install packages
 
-
-#dnf5 install -y blueman NetworkManager-tui nautilus waybar xdg-user-dirs-gtk xdg-user-dirs file-roller kitty btop dunst tlp zsh zsh-syntax-highlighting brightnessctl rofi-wayland --setopt=install_weak_deps=False 
-dnf5 install -y NetworkManager-tui nautilus file-roller kitty tlp zsh zsh-syntax-highlighting  --setopt=install_weak_deps=False 
-dnf5 remove -y gnome-disk-utility thunar  thunar-archive-plugin thunar-volman xarchiver android-tools wlsunset kanshi swaylock swayidle swaybg tuned tuned-ppd
+dnf5 install -y blueman NetworkManager-tui nautilus waybar xdg-user-dirs-gtk xdg-user-dirs file-roller kitty btop dunst tlp zsh zsh-syntax-highlighting brightnessctl tuigreet rofi-wayland --setopt=install_weak_deps=False 
 
 ## Enable Ublue copr
-#dnf5 -y copr enable ublue-os/akmods 
+dnf5 -y copr enable ublue-os/akmods 
 
 
 ## Hyprland
 dnf5 -y copr enable solopasha/hyprland 
-dnf5 -y install hyprland hyprpaper hypridle hyprlock hyprshot  --setopt=install_weak_deps=False
+dnf5 -y install hyprland hyprpaper hypridle hyprlock hyprpolkitagent hyprshot  --setopt=install_weak_deps=False
 dnf5 -y copr disable solopasha/hyprland 
 
 dnf5 -y copr enable tofik/nwg-shell 
@@ -30,7 +27,7 @@ dnf5 -y copr disable chenxiaolong/sbctl
 ## Tailscale
 dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/Fedora_Rawhide/shells:zsh-users:zsh-autosuggestions.repo
-dnf5 -y install tailscale zsh-autosuggestions greetd tuigreet
+dnf5 -y install tailscale zsh-autosuggestions greetd
 
 rm /etc/yum.repos.d/tailscale.repo
 rm /etc/yum.repos.d/shells:zsh-users:zsh-autosuggestions.repo
@@ -46,5 +43,5 @@ mkdir -p /nix && \
 
 systemctl enable tlp
 systemctl enable tailscaled
-#systemctl enable greetd
-rm -rf  /usr/etc
+systemctl enable greetd
+
